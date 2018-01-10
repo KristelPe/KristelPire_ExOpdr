de050ad52b7e72ae35bfe88655d2233b094a3745
Kristel <kristel_pire@hotmail.com>
2018-01-10 19:33:44 +0100

    fix - still not working

---
+++ KristelPire_ExOpdr/Services/ICarService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KristelPire_ExOpdr.Entities;
+
+namespace KristelPire_ExOpdr.Services
+{
+    public interface ICarService
+    {
+        List<Car> GetAllCars();
+        Car GetCarById(int id);
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int? id);
+        List<CarType> GetAllTypes();
+        CarType GetTypeById(int id);
+        void Persist(Car car);
+        void Delete(int id);
+    }
+}
---
+++ KristelPire_ExOpdr/Services/ITranslatorService.cs
@@ -0,0 +1,17 @@
+﻿using KristelPire_ExOpdr.Entities;
+using KristelPire_ExOpdr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KristelPire_ExOpdr.Services
+{
+    public interface ITranslatorService
+    {
+        CarDetailViewModel ConvertCar(Car car);
+        CarEditDetailViewModel ConvertEditCar(Car car);
+        OwnerDetailViewModel ConvertOwner(Owner owner);
+        TypeDetailViewModel ConvertType(CarType type);
+    }
+}
